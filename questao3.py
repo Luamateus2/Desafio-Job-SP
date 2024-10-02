36cd25e2be9ab9e720281dc4d1afb8a05469c45d
Lua Mateus <104106208+Luamateus2@users.noreply.github.com>
2024-10-02 15:14:32 -0300

    Update and rename faturamento_diario.py to questao3.py

--- questao3.py
+++ questao3.py
@@ -3,14 +3,15 @@ import json
 with open("dados.json", "r") as json_file:
     dados_faturamento = json.load(json_file)
 
-menor_valor = min(item["valor"] for item in dados_faturamento)
-maior_valor = max(item["valor"] for item in dados_faturamento)
-
 valores_faturamento = [item["valor"] for item in dados_faturamento if item["valor"] > 0]
+
+menor_valor = min(valores_faturamento)
+maior_valor = max(valores_faturamento)
+
 media_mensal = sum(valores_faturamento) / len(valores_faturamento)
 
 dias_acima_media = sum(1 for item in dados_faturamento if item["valor"] > media_mensal)
 
-print(f"Menor valor de faturamento diário: {menor_valor}")
-print(f"Maior valor de faturamento diário: {maior_valor}")
+print(f"Menor valor de faturamento diário: {menor_valor:.2f}")
+print(f"Maior valor de faturamento diário: {maior_valor:.2f}")
 print(f"Número de dias com faturamento acima da média mensal: {dias_acima_media}")
